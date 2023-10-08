1cd13d420b659144f10173919e3a970817816603
Kacper Knapczyk <88859642+Kacpi476@users.noreply.github.com>
2023-10-08 14:32:08 +0200

    Add files via upload

---
+++ plant.py
@@ -0,0 +1,10 @@
+namevar=input("enter my favourite plants name!:")
+plant="Spathiphyllum"
+smallplant="spathiphyllum"
+
+if plant == namevar:
+    print("Yes - Spathiphyllum is the best plant ever!")
+elif smallplant == namevar:
+    print("No, I want a big Spathiphyllum!")
+else :
+    print("Spathiphyllum! Not",namevar,"!")
