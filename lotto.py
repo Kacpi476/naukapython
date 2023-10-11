7a8a76d3854a6534ba80fee85112b1c3c390efc6
Kacpi476 <kapiszixd2@gmail.com>
2023-10-11 15:17:12 +0200

    nauka

---
+++ lotto.py
@@ -0,0 +1,15 @@
+import random
+
+liczby_lotto=[]
+
+for i in range(6):
+    liczba = random.randint(1,10)
+    liczby_lotto.append(liczba)
+    if liczba == liczby_lotto[]:
+        
+
+
+
+
+liczby_lotto.sort()
+print(liczby_lotto)
---
+++ zadanie_studia.py
@@ -0,0 +1,19 @@
+name = input('Enter your name: ')
+print('Hello {}'.format(name))
+
+
+hours = float(input('Enter hours: '))
+rate = float(input('Enter rate: '))
+print('Pay: {}'.format(hours*rate))
+
+width = 17
+height = 12.0
+
+print(width//2)
+print(width/2.0)
+print(height/3)
+print(1 + 2 * 5)
+
+celc = float(input('Wprowadź temperaturę w stopniach celcjusza'))
+fahr = (9 * celc ) / 5 + 32
+print(fahr)
